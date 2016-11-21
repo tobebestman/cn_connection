7e2e628f7c3c674c4a022d41c027e319ce54e808
JinGang.Li <tobebestman@gmail.com>
2016-11-21 00:19:35 -0800

    make some change

--- BridgeDoubleBrace/Resource/BridgeSingleBrace/copyToOut.bat
+++
@@ -1,2 +0,0 @@
-md "C:\Program Files (x86)\Bentley\ProStructures\V8i_S6\MicroStation 081109\Prg\Plugins\SingleLegBrace"
-copy "*.png" "C:\Program Files (x86)\Bentley\ProStructures\V8i_S6\MicroStation 081109\Prg\Plugins\SingleLegBrace\*.png" /Y
